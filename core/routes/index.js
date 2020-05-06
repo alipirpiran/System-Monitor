8150cd501d3e31c59775a00c80ae3d3d5dff6adc
Ali Pirpiran <alipirpiran@gmail.com>
2020-05-06 19:25:56 +0430

    init

--- core/routes/index.js
+++ core/routes/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const path = require('path')
 
 router.get('/', (req, res) => {
-    // res.sendFile(__dirname + '/frontend/index.html');
     res.sendFile(path.join(__dirname, '/../../frontend/index.html'))
 });
 
